2135e901804dda7ff6360eb2e8bf67d5ff40b7bd
Matt Dancho <mdancho@gmail.com>
2020-10-19 20:31:41 -0400

    rtip 008 - tidyverse relocate

---
+++ 008_tidyverse_new_features_relocate/008_tidyverse_relocate.R
@@ -0,0 +1,54 @@
+# R TIPS ----
+# TIP 008 | Must-Know Tidyverse Features: Relocate ----
+#
+# 👉 For Weekly R-Tips, Sign Up Here: https://mailchi.mp/business-science/r-tips-newsletter
+
+# LIBRARIES ----
+library(tidyverse)
+
+# DATA ----
+mpg
+
+# SELECT VS RELOCATE ----
+
+# - Select is like filter() for columns
+mpg %>%
+    select(model, manufacturer, class, year)
+
+# - Relocate is like arrange() for columns
+mpg  %>%
+    relocate(model, manufacturer, class, year)
+
+
+# 1.0 RELOCATE BY COLUMN NAME ----
+# - Move single column by position
+
+mpg %>%
+    relocate(manufacturer, .after = class)
+
+?last_col
+
+mpg %>%
+    relocate(manufacturer, .after = last_col())
+
+mpg %>%
+    relocate(manufacturer, .after = last_col(offset = 2))
+
+
+# 2.0 RELOCATE BY DATA TYPE ----
+# - Move multiple columns by data type
+
+mpg %>%
+    relocate(where(is.numeric))
+
+mpg %>%
+    relocate(where(is.character))
+
+mpg %>%
+    relocate(where(is.character), .after = last_col())
+
+
+# 3.0 RELOCATE WITH TIDYSELECT ----
+
+mpg %>%
+    relocate(contains("d"), .before = year)
