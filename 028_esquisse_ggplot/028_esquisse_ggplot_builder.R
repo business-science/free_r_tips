db4539f30ff4aee8316fb63c7fa2f85c6409a616
Matt Dancho <mdancho@gmail.com>
2021-03-15 18:04:01 -0400

    rtip 28

---
+++ 028_esquisse_ggplot/028_esquisse_ggplot_builder.R
@@ -0,0 +1,19 @@
+# R TIPS ----
+# TIP 028 | Esquisse ggplot builder ---
+#
+# 👉 For Weekly R-Tips, Sign Up Here: https://mailchi.mp/business-science/r-tips-newsletter
+
+# LIBRARIES ----
+
+library(esquisse)
+library(modeldata)
+library(tidyverse)
+
+# LOAD SOME DATA ----
+data("drinks")
+data("mpg")
+
+# RUN ESQUISSE ----
+esquisser()
+
+
