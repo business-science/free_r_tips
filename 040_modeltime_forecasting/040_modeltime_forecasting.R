e464820163ea7784c0fe8f93b66f578d20fd3bcd
Matt Dancho <mdancho@gmail.com>
2021-06-29 07:32:37 -0400

    finalize

--- 040_modeltime_forecasting/040_modeltime_forecasting.R
+++ 040_modeltime_forecasting/040_modeltime_forecasting.R
@@ -1,5 +1,5 @@
 # R TIPS ----
-# TIP 039 | Introduction to Modeltime: In 2-Minutes ----
+# TIP 039 | Introduction to Modeltime: In 5-Minutes ----
 #
 # 👉 For Weekly R-Tips, Sign Up Here:
 #    https://mailchi.mp/business-science/r-tips-newsletter
@@ -19,23 +19,35 @@ bike_sharing_daily
 data <- bike_sharing_daily %>%
     select(dteday, cnt)
 
-data
-
 data %>% plot_time_series(dteday, cnt)
+
+# TRAIN / TEST SPLITS ----
+
+splits <- time_series_split(
+    data,
+    assess     = "3 months",
+    cumulative = TRUE
+)
+
+splits %>%
+    tk_time_series_cv_plan() %>%
+    plot_time_series_cv_plan(dteday, cnt)
 
 # FORECAST ----
 
-# * ARIMA ---
-model_arima <- arima_reg(seasonal_period = 52) %>%
+# * AUTO ARIMA ---
+model_arima <- arima_reg() %>%
     set_engine("auto_arima") %>%
-    fit(cnt ~ dteday, data)
+    fit(cnt ~ dteday, training(splits))
 
 model_arima
 
 # * Prophet ----
-model_prophet <- prophet_reg() %>%
+model_prophet <- prophet_reg(
+        seasonality_yearly = TRUE
+    ) %>%
     set_engine("prophet") %>%
-    fit(cnt ~ dteday, data)
+    fit(cnt ~ dteday, training(splits))
 
 model_prophet
 
@@ -43,21 +55,54 @@ model_prophet
 model_glmnet <- linear_reg(penalty = 0.01) %>%
     set_engine("glmnet") %>%
     fit(
-        cnt ~ month(dteday, label = TRUE) + wday(dteday, label = TRUE) + as.numeric(dteday),
-        data
+        cnt ~ wday(dteday, label = TRUE)
+            + month(dteday, label = TRUE)
+            + as.numeric(dteday),
+        training(splits)
     )
+
+model_glmnet
+
 
 # MODELTIME COMPARE ----
 
+# * Modeltime Table ----
 model_tbl <- modeltime_table(
     model_arima,
     model_prophet,
     model_glmnet
 )
 
-model_tbl %>%
+# * Calibrate ----
+calib_tbl <- model_tbl %>%
+    modeltime_calibrate(testing(splits))
+
+# * Accuracy ----
+calib_tbl %>% modeltime_accuracy()
+
+# * Test Set Visualization ----
+calib_tbl %>%
     modeltime_forecast(
-        h           = "1 year",
+        new_data    = testing(splits),
         actual_data = data
     ) %>%
     plot_modeltime_forecast()
+
+# * Forecast Future ----
+future_forecast_tbl <- calib_tbl %>%
+    modeltime_refit(data) %>%
+    modeltime_forecast(
+        h           = "3 months",
+        actual_data = data
+    )
+
+future_forecast_tbl %>%
+    plot_modeltime_forecast()
+
+# LEARNING MORE ----
+# 1. FEATURE ENGINEERING - A LOT OF ROOM FOR IMPROVEMENT
+# 2. MACHINE LEARNING ALGORIMTHMS | TIDYMODELS API
+# 3. MODELTIME ECOSYSTEM | MODELTIME H2O, GLUONTS
+
+# HIGH PERFORMANCE TIME SERIES COURSE:
+# https://university.business-science.io/p/ds4b-203-r-high-performance-time-series-forecasting/
