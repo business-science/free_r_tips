4e7aed93aa5bff2f98b85dd717bc3a4c019deb91
Matt Dancho <mdancho@gmail.com>
2022-02-01 09:25:30 -0500

    48 - radiant

---
+++ 048_radiant/048_radiant.R
@@ -0,0 +1,29 @@
+# R TIPS ----
+# TIP 048| radiant: Business Intelligence with R  ----
+#
+# 👉 For Weekly R-Tips, Sign Up Here:
+#    https://mailchi.mp/business-science/r-tips-newsletter
+
+# Radiant Documentation: https://radiant-rstats.github.io/docs/
+
+# INSTALLATION ----
+# install.packages("radiant", repos = "https://radiant-rstats.github.io/minicran/", type = "binary")
+
+
+# LIBRARIES ----
+
+library(radiant)
+library(timetk)
+
+data(walmart_sales_weekly)
+
+# RUN RADIANT ----
+
+radiant()
+
+# LEARNING MORE ----
+
+# FREE MASTERCLASS
+# - 10 SECRETS TO BECOMING A DATA SCIENTIST
+#   https://mailchi.mp/business-science/rtrack-master-class-signup-3
+
