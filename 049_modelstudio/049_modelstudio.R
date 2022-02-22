f912a930eb0230fed2634525fa8e50f1ff9e022e
Matt Dancho <mdancho@gmail.com>
2022-02-21 21:38:13 -0500

    post recording

---
+++ 049_modelstudio/049_modelstudio.R
@@ -0,0 +1,52 @@
+# R TIPS ----
+# TIP 049| ModelStudio: Interactive Studio for Explanatory Model Analysis  ----
+#
+# 👉 For Weekly R-Tips, Sign Up Here:
+#    https://mailchi.mp/business-science/r-tips-newsletter
+
+# Documentation: https://modelstudio.drwhy.ai/index.html
+
+# INSTALLATION ----
+# install.packages("modelStudio")
+
+
+# LIBRARIES ----
+
+library(modelStudio)
+library(DALEX)
+library(tidyverse)
+library(tidymodels)
+
+# DATA ----
+
+data_tbl <- mpg %>%
+    select(hwy, manufacturer:drv, fl, class)
+
+# MODEL ----
+
+fit_xgboost <- boost_tree(learn_rate = 0.3) %>%
+    set_mode("regression") %>%
+    set_engine("xgboost") %>%
+    fit(hwy ~ ., data = data_tbl)
+
+fit_xgboost
+
+# EXPLAINER ----
+
+explainer <- DALEX::explain(
+    model = fit_xgboost,
+    data  = data_tbl,
+    y     = data_tbl$hwy,
+    label = "XGBoost"
+)
+
+# MODEL STUDIO ----
+
+modelStudio::modelStudio(explainer)
+
+# LEARNING MORE ----
+
+# FREE MASTERCLASS
+# - 10 SECRETS TO BECOMING A DATA SCIENTIST
+#   https://mailchi.mp/business-science/rtrack-master-class-signup-3
+
