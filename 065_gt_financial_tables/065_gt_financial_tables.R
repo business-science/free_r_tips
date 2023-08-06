6d07fa46fe095c71a5ad9b94fe831d5a92dc3ae2
Matt Dancho <mdancho@gmail.com>
2023-08-06 09:22:44 -0400

    clean up code

--- 065_gt_financial_tables/065_gt_financial_tables.R
+++ 065_gt_financial_tables/065_gt_financial_tables.R
@@ -60,32 +60,32 @@ cashflow_wide_tbl %>%
     gt() %>%
     gtExtras::gt_plt_sparkline(trend) %>%
     tab_header(
-        title = "Cash Flow Statement",
+        title    = "Cash Flow Statement",
         subtitle = "Exxon Mobil (FY2009 - FY2018)"
     ) %>%
     tab_spanner(
-        label = "Fiscal Year (Values in Millions)",
+        label   = "Fiscal Year (Values in Millions)",
         columns = fy_09:fy_18,
     ) %>%
     cols_label(
-        item_id = "Item No.",
+        item_id   = "Item No.",
         item_name = "Item Name",
         item_type = "Item Type",
-        fy_09 = "2009",
-        fy_10 = "2010",
-        fy_11 = "2011",
-        fy_12 = "2012",
-        fy_13 = "2013",
-        fy_14 = "2014",
-        fy_15 = "2015",
-        fy_16 = "2016",
-        fy_17 = "2017",
-        fy_18 = "2018",
-        trend = "Trend"
+        fy_09     = "2009",
+        fy_10     = "2010",
+        fy_11     = "2011",
+        fy_12     = "2012",
+        fy_13     = "2013",
+        fy_14     = "2014",
+        fy_15     = "2015",
+        fy_16     = "2016",
+        fy_17     = "2017",
+        fy_18     = "2018",
+        trend     = "Trend"
     ) %>%
     fmt_currency(
-        columns  = fy_09:fy_18,
-        decimals = 0,
+        columns    = fy_09:fy_18,
+        decimals   = 0,
         accounting = TRUE
     ) %>%
     cols_align(align = "center") %>%
@@ -95,10 +95,10 @@ cashflow_wide_tbl %>%
     ) %>%
     cols_hide(columns = c(item_type, group_id)) %>%
     tab_options(
-        heading.title.font.size = 20,
-        table.font.size = 13,
+        heading.title.font.size    = 20,
+        table.font.size            = 13,
         heading.subtitle.font.size = 12,
-        column_labels.font.weight = "bold",
+        column_labels.font.weight  = "bold",
     )
 
 
