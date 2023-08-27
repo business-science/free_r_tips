ba27e6812313e3adb22f322e7b29d1d90295454f
Matt Dancho <mdancho@gmail.com>
2023-08-27 11:01:29 -0400

    post recording

--- 067_types_of_data_professionals/067_types_of_data_professionals_plot.R
+++ 067_types_of_data_professionals/067_types_of_data_professionals_plot.R
@@ -41,7 +41,7 @@ basic_radar_plot <- data_prep_tbl %>%
     ggplot(aes(name, value, group = type)) +
     geom_polygon(aes(fill = type, color = type), alpha = 0.25) +
     geom_point(aes(color = type)) +
-    scale_y_continuous(limits = c(-1, 12), breaks = c(0, 2.5, 5, 7.5, 10)) +
+    scale_y_continuous(limits = c(-1, 13), breaks = c(0, 2.5, 5, 7.5, 10)) +
     coord_radar(start = -0.3, clip = "off") +
     facet_wrap(~ type)
 
@@ -54,6 +54,7 @@ basic_radar_plot  +
     scale_color_manual(values = c("#588f3a", "#E7B800", "#00AFBB", "#FC4E07")) +
     labs(
         title = "Types of Data Professionals",
+        subtitle = "Which are you?",
         legend = NULL,
         x = NULL,
         y = NULL
@@ -61,21 +62,32 @@ basic_radar_plot  +
     theme(
         plot.title = element_text(
             hjust = 0.5,
-            margin = margin(t = 20, b = 20),
-            size = 25,
+            margin = margin(t = 5, b = 20),
+            size = 28,
             face = "bold",
             family = "serif"
         ),
+        plot.subtitle = element_text(
+            color = "dodgerblue",
+            hjust = 0.5,
+            margin = margin(t = 0, b = 20),
+            size = 20,
+            face = "bold",
+
+        ),
         strip.text = element_text(
-            size = 15,
+            size = 18,
             face = "bold",
             margin = margin(t = 5, b = 5)
         ),
         legend.position = "none",
         axis.text.y = element_blank(),
-        axis.text.x = element_text(vjust = -1),
+        axis.text.x = element_text(
+            vjust = -1,
+            size = 8
+        ),
         panel.spacing = unit(50, "points"),
-        # panel.grid.minor = element_blank(),
+        panel.grid.minor = element_blank(),
         panel.grid.major.y = element_line(colour = c(rep("#ebebeb", 5), NA))
     )
 
