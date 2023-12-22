771007341eff4a7e5c928c2e33a60b2e92f9a40e
Matt Dancho <mdancho@gmail.com>
2023-12-22 09:21:29 -0500

    initial commit

---
+++ 074_ggplot_ggstream_showtext/074_ggplot_showtext.R
@@ -0,0 +1,106 @@
+
+
+
+# Libraries ----
+library(tidyverse)
+library(timetk)
+library(ggstream) # smooth geom area shapes
+library(showtext) # load custom fonts
+library(ggtext)
+library(tidyquant)
+
+# Data ----
+# * ONE OF THE BUSINESS DATASETS FROM THE R-TRACK PROGRAM
+transactions_raw_tbl <- read_csv("074_ggplot_ggstream_showtext/data/bike_orderlines.csv")
+
+transactions_raw_tbl %>% glimpse()
+
+# Data Wrangling ----
+
+sales_by_category2_y_tbl <- transactions_raw_tbl %>%
+    mutate(total_sales = quantity * price) %>%
+    group_by(category_2) %>%
+    summarise_by_time(
+        .by = "year",
+        total_sales = sum(total_sales)
+    ) %>%
+    ungroup() %>%
+    mutate(category_2 = fct_reorder2(category_2, order_date, total_sales))
+
+# VISUALIZATION ----
+
+# Step 1: Basic Stacked Area ----
+
+sales_by_category2_y_tbl %>%
+    ggplot(aes(order_date, total_sales, fill = category_2, color = category_2, label=category_2)) +
+    geom_area()
+
+# Step 2: Geom Stream ----
+
+sales_by_category2_y_tbl %>%
+    ggplot(aes(order_date, total_sales, fill = category_2, color = category_2, label=category_2)) +
+    geom_stream(type = "ridge", bw = 1.1, extra_span = 0.10)
+
+# Step 3: Improve the appearance -----
+
+sales_by_category2_y_tbl %>%
+    ggplot(aes(order_date, total_sales, fill = category_2, color = category_2, label=category_2)) +
+    geom_stream(type = "ridge", bw = 1.1, extra_span = 0.10) +
+
+    # Theme
+    scale_color_tq() +
+    scale_fill_tq() +
+    theme_tq()
+
+# Step 4: Better Fonts -----
+
+font_add_google(name = "Josefin Sans", family = "Josefin_Sans")
+
+sales_by_category2_y_tbl %>%
+    ggplot(aes(order_date, total_sales, fill = category_2, color = category_2, label=category_2)) +
+    geom_stream(type = "ridge", bw = 1.1, extra_span = 0.10) +
+
+    # Theme
+    scale_color_tq() +
+    scale_fill_tq() +
+    theme_tq(base_family = "Josefin_Sans")
+
+# Step 5: Advanced Annotation ----
+
+sales_by_category2_y_tbl %>%
+    ggplot(aes(order_date, total_sales, fill = category_2, color = category_2, label=category_2)) +
+    geom_stream(type = "ridge", bw = 1.1, extra_span = 0.10) +
+    labs(x = "", y = "") +
+
+    # Theme
+    scale_color_tq() +
+    scale_fill_tq() +
+    theme_tq(base_family = "Josefin_Sans") +
+
+    # Advanced Theming
+    theme(
+        legend.position = "none",
+        axis.line.x = element_line(linewidth = .75),
+        panel.grid = element_blank(),
+        axis.text.y = element_blank(),
+        panel.border = element_blank(),
+        plot.margin = margin(20,120,20,20),
+    ) +
+
+    # Advanced Annotations
+    expand_limits(y = 2.3e7) +
+
+    # Title
+    annotate(
+        "text",
+        label = "Cannondale Bicycle\nSales",
+        x = parse_date2("2011-01-01") %>% as_datetime(),
+        y = 2.1e7,
+        size = 13,
+        hjust = 0,
+        vjust = 1,
+        family = "Josefin_Sans"
+    )
+
+    #
+
