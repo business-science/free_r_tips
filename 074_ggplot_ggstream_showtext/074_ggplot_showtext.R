e2c704264d7a5e0563b7cb6790041e9d8fbb3bf7
Matt Dancho <mdancho@gmail.com>
2023-12-22 11:08:28 -0500

    finalize plot

--- 074_ggplot_ggstream_showtext/074_ggplot_showtext.R
+++ 074_ggplot_ggstream_showtext/074_ggplot_showtext.R
@@ -1,12 +1,24 @@
+# BUSINESS SCIENCE R TIPS ----
+# R-TIP 74 | Advanced ggplot2 visualizations: ggstream and showtext ----
 
+# 👉 For Weekly R-Tips, Sign Up Here:
+#    https://learn.business-science.io/r-tips-newsletter
+
+# 👉 Shoutout:
+#  https://r-graph-gallery.com/web-stacked-area-chart-inline-labels.html
+
+# 👉 R-Track Masterclass:
+# - Do you want to become the data science expert for your organization?
+# - HERE'S HOW: 10 SECRETS TO BECOMING A 6-FIGURE DATA SCIENTIST
+#   https://learn.business-science.io/free-rtrack-masterclass
 
 
 # Libraries ----
 library(tidyverse)
 library(timetk)
-library(ggstream) # smooth geom area shapes
-library(showtext) # load custom fonts
-library(tidyquant) # tq theme
+library(ggstream)  # smooth geom area shapes
+library(showtext)  # load custom fonts
+library(tidyquant) # tidyquant color theme
 
 # Custom Fonts ----
 # * Run this to enable showtext fonts used in this tutorial:
@@ -33,8 +45,8 @@ sales_by_category2_y_tbl <- transactions_raw_tbl %>%
     mutate(category_2 = fct_reorder2(category_2, order_date, total_sales)) %>%
     arrange(category_2)
 
-# * Label Data ----
-sales_by_category_2_total_tbl <- sales_by_category2_y_tbl %>%
+# * Y-Label Data ----
+y_label_tbl <- sales_by_category2_y_tbl %>%
     group_by(category_2) %>%
     summarise(
         order_date = max(order_date),
@@ -43,11 +55,21 @@ sales_by_category_2_total_tbl <- sales_by_category2_y_tbl %>%
     mutate(category_2 = fct_reorder2(category_2, order_date, total_sales_all)) %>%
     arrange(desc(category_2)) %>%
     mutate(
-        midpoint = c(0, 0.5e6, 1e6, 2e6, 3e6, 4.7e6, 7.5e6, 1.1e7, 1.5e7)
+        midpoint = c(0, 0.5e6, 1e6, 2e6, 3e6, 4.9e6, 7.5e6, 1.1e7, 1.5e7)
     ) %>%
     mutate(
-        text = str_glue("{category_2} {scales::dollar(total_sales_all, suffix = 'M', scale = 1/1e6)}")
+        y_text = str_glue("{category_2} {scales::dollar(total_sales_all, suffix = 'M', scale = 1/1e6)}")
     )
+
+# * X-Label Data ----
+
+x_label_tbl <- sales_by_category2_y_tbl %>%
+    group_by(order_date) %>%
+    summarize(
+        total_sales_all = sum(total_sales),
+    ) %>%
+    mutate(points = c(12492885, 13513075, 16030775, 15424085, 18971510)) %>%
+    mutate(x_text = scales::dollar(total_sales_all))
 
 # ADVANCED VISUALIZATION TUTORIAL ----
 
@@ -84,13 +106,17 @@ sales_by_category2_y_tbl %>%
     # Theme
     scale_color_tq() +
     scale_fill_tq() +
-    theme_tq(base_family = "Josefin_Sans")
+    theme_tq(base_family = "Josefin_Sans") +
 
-# Step 5: Advanced Annotation ----
+    labs(title= "Cannondale Bicycle Sales")
+
+
+
+# BONUS: Step 5: Advanced Annotation and Theming ----
 
 sales_by_category2_y_tbl %>%
-    ggplot(aes(order_date, total_sales, fill = category_2, color = category_2, label=category_2)) +
-    geom_stream(type = "ridge", bw = 1.1, extra_span = 0.10) +
+    ggplot(aes(order_date, total_sales)) +
+    geom_stream(aes(fill = category_2, color = category_2), type = "ridge", bw = 1.1, extra_span = 0.10) +
     labs(x = "", y = "") +
 
     # Theme
@@ -106,30 +132,56 @@ sales_by_category2_y_tbl %>%
         panel.grid = element_blank(),
         axis.text.y = element_blank(),
         panel.border = element_blank(),
-        plot.margin = margin(20,160,20,20),
+        plot.margin = margin(0,160,20,20),
     ) +
 
     # Advanced Annotations
-    expand_limits(y = 2.3e7) +
+    expand_limits(y = 2.5e7) +
 
     # Title
     annotate(
         "text",
         label = "Cannondale Bicycle\nSales",
         x = parse_date2("2011-01-01") %>% as_datetime(),
-        y = 2.1e7,
+        y = 2.4e7,
         size = 13,
         hjust = 0,
         vjust = 1,
-        family = "Josefin_Sans"
+        family = "Josefin_Sans",
+        fontface = "bold"
     ) +
 
     # Y-Axis Labels (Legend)
     geom_text(
-        aes(x = (order_date + days(30)) , y = midpoint, label = text),
+        aes(x = (order_date + days(30)) , y = midpoint, label = y_text, color = category_2),
         hjust = 0,
         family = "Josefin_Sans",
-        data = sales_by_category_2_total_tbl
+        fontface = "bold",
+        size = 3,
+        data = y_label_tbl
+    ) +
+
+    # X-Axis Labels (Points)
+    geom_point(
+        aes(x = order_date, y = points),
+        color = "black",
+        fill = "black",
+        data = x_label_tbl
+    ) +
+    geom_segment(
+        aes(xend = order_date, x = order_date, yend = 0, y = points),
+        color = "black",
+        size = 0.75,
+        data = x_label_tbl
+    ) +
+    geom_text(
+        aes(x = order_date, y = points, label = x_text),
+        # hjust = 0,
+        nudge_y = 5e5,
+        family = "Josefin_Sans",
+        fontface = "bold",
+        size = 3,
+        data = x_label_tbl
     )
 
 
